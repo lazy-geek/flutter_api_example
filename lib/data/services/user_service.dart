016f0af8f83f9c62a8696a6a8fff118fc438d2f1
abarar <38218691+lazy-geek@users.noreply.github.com>
2022-05-29 15:15:16 +0530

    Add updateUser function And .env variable for api key

--- lib/data/services/user_service.dart
+++ lib/data/services/user_service.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 
 import 'package:flutter/foundation.dart';
 import 'package:flutter_api_example/data/models/user.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 import 'package:http/http.dart' as http;
 
@@ -10,23 +11,39 @@ class UserService {
   static final UserService instance = UserService._();
 
   static const String baseUrl = 'https://gorest.co.in/public/v2';
-
+//  dotenv.env['API_URL']
   Future<List<User>> getAllUsers() async {
     try {
-      http.Response response = await http.get(Uri.parse('${baseUrl}/users'));
+      http.Response response = await http.get(Uri.parse('${baseUrl}/users'),
+          headers: {'Authorization': 'Bearer ${dotenv.env["API_KEY"]}'});
       if (response.statusCode != 200) throw Exception('bad status code');
       var data = jsonDecode(response.body);
       List<User> users = [];
       for (var item in data) {
         users.add(User.fromJson(item));
       }
-
       return users;
     } catch (e) {
       if (kDebugMode) {
         print(e.toString());
       }
       return [];
+    }
+  }
+
+  Future<void> updateUser(User user) async {
+    try {
+      http.Response response = await http.put(
+          Uri.parse('${baseUrl}/users/${user.id}'),
+          body: user.toJson(),
+          headers: {'Authorization': 'Bearer ${dotenv.env["API_KEY"]}'});
+      if (response.statusCode != 200) throw Exception('bad status code');
+      print(response.body);
+      var data = jsonDecode(response.body);
+    } catch (e) {
+      if (kDebugMode) {
+        print(e.toString());
+      }
     }
   }
 }
