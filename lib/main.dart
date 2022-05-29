d5ceab6f66abf682c9abc6c692842de24c183e1b
abarar <38218691+lazy-geek@users.noreply.github.com>
2022-05-29 15:16:15 +0530

    setup .env in main functiion

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_api_example/presentation/pages/home_page.dart';
 import 'package:flutter_api_example/presentation/pages/login_page.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-void main() {
+Future<void> main() async {
+  await dotenv.load();
+
   runApp(const MyApp());
 }
 
